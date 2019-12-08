f5a752797db024d439d6ed7764957b589a60bba3
Jonas Heinle <jonasheinle@googlemail.com>
2019-12-08 11:46:39 +0100

    retargeting project further pushed

--- Retargeting/Retargeting/helpers.h
+++ Retargeting/Retargeting/helpers.h
@@ -1,6 +1,11 @@
 #pragma once
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
+#include "d3d11.h"
 
-class helpers {
-	bool LoadTextureFromFromFile(const char* filename, int* width, int* height);
-};
+	class helpers {
+	public:
+		helpers() {};
+		bool LoadTextureFromFile(const char* filename, ID3D11ShaderResourceView** srv, ID3D11Device* g_pd3dDevice, int* width, int* height);
+	};
+	
