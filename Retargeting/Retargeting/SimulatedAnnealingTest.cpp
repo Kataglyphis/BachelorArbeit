a80802f6456e2a0e454990c99afc3a210e917dae
jonasheinle <jonasheinle@googlemail.com>
2020-01-16 13:01:42 +0100

    runned once

--- Retargeting/Retargeting/SimulatedAnnealingTest.cpp
+++ Retargeting/Retargeting/SimulatedAnnealingTest.cpp
@@ -1,7 +1,7 @@
 #include "SimulatedAnnealingTest.h";
 
 
-SimulatedAnnealingTest::SimulatedAnnealingTest() : helper(), number_steps(100), sa(100) {
+SimulatedAnnealingTest::SimulatedAnnealingTest() : helper(), number_steps(1000), sa(1000) {
 
 }
 
